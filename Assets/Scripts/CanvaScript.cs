70a322b17bacff0c788fc4ed5160f37ede562803
6uara <juanguaragnini@gmail.com>
2024-05-02 15:49:38 -0300

    started observer

---
+++ Assets/Scripts/CanvaScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvaScript : MonoBehaviour
+{
+    [SerializeField] private Player pj; //deberia ser el lvl manager, no el pj
+    [SerializeField] private GameObject Time;
+    [SerializeField] private GameObject Damage;
+    [SerializeField] private GameObject Points;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pj.OnPointsUpdate += updatePoints;// deberia ser el lvl manager, no el Pj
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void updatePoints(){}
+    public void updateTime(){}
+    public void updateDamage(){}
+}
